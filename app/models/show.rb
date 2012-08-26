c8e7e2bfa32158dbaf00a0ac836c9ee54beff78f
Fox <fox@thefox.eu>
2012-08-26 15:08:37 +0100

    * full blown show class

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,25 @@
 class Show < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :season, :episode, :runtime, :hd, :image_url, :next_show_date
+  
+  validates :name,  :presence => true
+  
+  def to_s()
+    return "Show[name=#{@name},season=#{@season},episode=#{@episode}]"
+  end
+
+  def generate_search_string()
+    return "#{@name} #{generate_hd_string()} #{generate_episode_string()}"
+  end
+
+  def generate_hd_string()
+    if hd
+      return "720p"
+    else
+      return ""
+    end
+  end
+
+  def generate_episode_string()
+    return "S%02dE%02d" % [season, episode]
+  end
 end
