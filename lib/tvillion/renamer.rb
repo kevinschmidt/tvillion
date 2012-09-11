4cb153970ed55e295b7b50987650ab2ba81ddafc
Fox <fox@thefox.eu>
2012-09-11 22:08:07 +0100

    adding test for all tree version and 720p detection

--- lib/tvillion/renamer.rb
+++ lib/tvillion/renamer.rb
@@ -1,7 +1,7 @@
 module TVillion
   module Renamer
-    STANDARD = Regexp.new('^(.*)[ ._-][Ss](\d{2})[ ._-]?[Ee](\d{1,2})[ ._-](.*)[.](\w{3,4})$')
-    LONG_NAMES = Regexp.new('^(.*)[Ss]eason[ .]([1-9]{1,2})[ .][Ee]pisode[ .](\d{2})(.*)[.](\w{3,4})')
+    STANDARD = Regexp.new('^(?<showname>.*)[ ._-][Ss](?<seasonnum>\d{1,2})[ ._-]?[Ee](?<episodenum>\d{1,2})[ ._-](?<episodename>.*)[.](?<fileend>\w{3,4})$')
+    LONG_NAMES = Regexp.new('^(?<showname>.*)[Ss]eason[ .](?<seasonnum>[0-9]{1,2})[ .][Ee]pisode[ .](?<episodenum>\d{2})(?<episodename>.*)[.](?<fileend>\w{3,4})')
     JUST_NUMBERS = Regexp.new('^(?<showname>.*)(?<seasonnum>[0-9]{1,2})(?<episodenum>\d{2})(?<episodename>.*)[.](?<fileend>\w{3,4})$')
     REGEX_ARRAY = [STANDARD, LONG_NAMES, JUST_NUMBERS]
     
@@ -16,14 +16,20 @@ module TVillion
       end
       matchData = matchResults.first[0]
       result = Hash[ matchData.names.zip(matchData.captures) ]
+      
+      is720p = false
       result.each do |key, value|
         mod_value = value.gsub(/\A[_\W]+|[_\W]+\Z/, '')
         is_number = true if Fixnum(mod_value) rescue false
         if ['seasonnum', 'episodenum'].include?(key)
           mod_value = mod_value.rjust(2, '0')
         end
+        if key == 'episodename' && value.index(/720[pP]/)
+          is720p = true
+        end
         result[key] = mod_value
       end
+      result['720p'] = is720p
       return result
     end
   end
--- spec/renamer_spec.rb
+++ spec/renamer_spec.rb
@@ -10,8 +10,18 @@ describe TVillion::Renamer do
       @renamer = RenameTest.new
     end
     
-    it "should parse standard name" do
+    it "should parse just number name" do
       matchData = @renamer.matchName("Daria - 406 - I Loathe A Parade.mpg")
+      puts matchData
+    end
+    
+    it "should parse long name" do
+      matchData = @renamer.matchName("24 Season 1 Episode 10 - 9AM - 10AM.avi")
+      puts matchData
+    end
+    
+    it "should parse standard name" do
+      matchData = @renamer.matchName("Futurama.S07E01.720p.HDTV.x264-IMMERSE.mkv")
       puts matchData
     end
   end
