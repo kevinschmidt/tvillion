9574ba1c347ad32c604b9f7071d49fc344480cd7
Fox <fox@thefox.eu>
2012-09-11 22:54:31 +0100

    * working normalization

--- lib/tvillion/renamer.rb
+++ lib/tvillion/renamer.rb
@@ -4,9 +4,18 @@ module TVillion
     LONG_NAMES = Regexp.new('^(?<showname>.*)[Ss]eason[ .](?<seasonnum>[0-9]{1,2})[ .][Ee]pisode[ .](?<episodenum>\d{2})(?<episodename>.*)[.](?<fileend>\w{3,4})')
     JUST_NUMBERS = Regexp.new('^(?<showname>.*)(?<seasonnum>[0-9]{1,2})(?<episodenum>\d{2})(?<episodename>.*)[.](?<fileend>\w{3,4})$')
     REGEX_ARRAY = [STANDARD, LONG_NAMES, JUST_NUMBERS]
+    OUTPUT_FORMAT_SD = '%{showname}.S%{seasonnum}E%{episodenum}.%{fileend}'
+    OUTPUT_FORMAT_HD = '%{showname}.S%{seasonnum}E%{episodenum}.720p.%{fileend}'
+    
     
     def normalizeName(name)
       match_data = matchName(name)
+      symbolized_data = match_data.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+      if symbolized_data[:is720p]
+        return OUTPUT_FORMAT_HD % symbolized_data
+      else
+        return OUTPUT_FORMAT_SD % symbolized_data
+      end
     end
     
     def matchName(name)
@@ -19,7 +28,7 @@ module TVillion
       
       is720p = false
       result.each do |key, value|
-        mod_value = value.gsub(/\A[_\W]+|[_\W]+\Z/, '')
+        mod_value = value.gsub(/\A[_\W]+|[_\W]+\Z/, '').squeeze().gsub(/[ _]/, '.')
         is_number = true if Fixnum(mod_value) rescue false
         if ['seasonnum', 'episodenum'].include?(key)
           mod_value = mod_value.rjust(2, '0')
@@ -29,7 +38,7 @@ module TVillion
         end
         result[key] = mod_value
       end
-      result['720p'] = is720p
+      result['is720p'] = is720p
       return result
     end
   end
--- spec/renamer_spec.rb
+++ spec/renamer_spec.rb
@@ -5,7 +5,7 @@ describe TVillion::Renamer do
     include TVillion::Renamer
   end
 
-  context "renaming" do
+  context "matching" do
     before(:each) do
       @renamer = RenameTest.new
     end
@@ -21,8 +21,29 @@ describe TVillion::Renamer do
     end
     
     it "should parse standard name" do
-      matchData = @renamer.matchName("Futurama.S07E01.720p.HDTV.x264-IMMERSE.mkv")
+      matchData = @renamer.matchName("Modern Family.S03E18.720p.HDTV.X264-DIMENSION.mkv")
       puts matchData
+    end
+  end
+  
+  context "normalizing" do
+    before(:each) do
+      @renamer = RenameTest.new
+    end
+    
+    it "should rename just number name" do
+      newName = @renamer.normalizeName("Daria - 406 - I Loathe A Parade.mpg")
+      newName.should eq("Daria.S04E06.mpg")
+    end
+    
+    it "should rename long name" do
+      newName = @renamer.normalizeName("24 Season 1 Episode 10 - 9AM - 10AM.avi")
+      newName.should eq("24.S01E10.avi")
+    end
+    
+    it "should rename standard name" do
+      newName = @renamer.normalizeName("Modern Family.S03E18.720p.HDTV.X264-DIMENSION.mkv")
+      newName.should eq("Modern.Family.S03E18.720p.mkv")
     end
   end
 end
