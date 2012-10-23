768276f0d3e09a8e6280c751a88a85d2cbee4865
Fox <fox@thefox.eu>
2012-10-24 00:22:35 +0100

    * better tables and ajax loader

--- app/assets/javascripts/shows.js
+++ app/assets/javascripts/shows.js
@@ -1,8 +1,9 @@
 $(function() {
+  $('#shows_main_table').tablesorter();
   $('#search_result_table').tablesorter();
-  //$('#search_result_table').tablesorter({ sortList: [[1,0]] });
 
   $('#shows_search_lookup_form').submit(function() {
+    $('#notice').text("");
     $.get(this.action, $(this).serialize(), null, 'script');
     $('#search_result_table').trigger('update');
     return false;
@@ -10,12 +11,20 @@ $(function() {
 
   $('#search_result_table tr').die('click').live('click', function() {
     var id = $(this).find('td.id').text();
-    var name = $(this).find('td.name').text();
-    reply=confirm('Create show ' + name + ' with tvrage id ' + id + '?');
-    if (reply) {
-      $.post("/shows.json", { "show[name]" : $(this).find('td.name').text(), "show[tvrage_id]" : $(this).find('td.id').text(), "show[hd]" : "true" }, function(response) {
-        $('#notice').text("Created show " + response.name + " with tvrage id " + response.tvrage_id + " and tvillion id " + response.id + ".");
-      }, 'json');
+    if (id) {
+      var name = $(this).find('td.name').text();
+      reply=confirm('Create show ' + name + ' with tvrage id ' + id + '?');
+      if (reply) {
+        $.post("/shows.json", { "show[name]" : $(this).find('td.name').text(), "show[tvrage_id]" : $(this).find('td.id').text(), "show[hd]" : "true" }, function(response) {
+          $('#notice').text("Created show " + response.name + " with tvrage id " + response.tvrage_id + " and tvillion id " + response.id + ".");
+        }, 'json');
+      }
     }
+  });
+
+  $('#loading-indicator').hide().ajaxStart(function() {
+    $(this).show();
+  }).ajaxStop(function() {
+    $(this).hide();
   });
 });
