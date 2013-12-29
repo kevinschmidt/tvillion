28b59cbf6d606f4791acc6713dbdbe789e616333
Fox <fox@thefox.eu>
2013-12-28 22:17:43 -0500

    adding fenopy parsing and making it the default

--- lib/tvillion/torrentsearch.rb
+++ lib/tvillion/torrentsearch.rb
@@ -3,15 +3,31 @@ require 'net/http'
 
 module TVillion
   module TorrentSearch
+    def get_search_results(search_string)
+      search_fenopy(search_string)
+    end
+
     THORRENTS_URL = "http://thorrents.com/search/%s.json"
     
-    def get_search_results(search_string)
+    def search_thorrents(search_string)
       resp = Net::HTTP.get_response(URI.parse(URI.escape(THORRENTS_URL % search_string)))
       data = resp.body
       result = JSON.parse(data)
       if result.has_key? 'results' and not result['results'].empty?
         items = result['results']
         return items[0]['magnet']
+      end
+      return nil
+    end
+    
+    FENOPY_URL = "http://unblockfenopy.eu/module/search/api.php?keyword=%s&sort=relevancy&format=json&limit=10&category=78"
+    
+    def search_fenopy(search_string)
+      resp = Net::HTTP.get_response(URI.parse(URI.escape(FENOPY_URL % search_string)))
+      data = resp.body
+      result = JSON.parse(data)
+      if not result.empty?
+        return result[0]['magnet']
       end
       return nil
     end
--- spec/torrentsearch_spec.rb
+++ spec/torrentsearch_spec.rb
@@ -12,13 +12,22 @@ describe TVillion::TorrentSearch do
       @torrent_search = TorrentSearchTest.new
     end
 
-    it "should parse the search json and get a proper torrent url as a result" do
+    it "should parse thorrents search json and get a proper torrent url as a result" do
       Net::HTTP.should_receive(:get_response).with(URI.parse(URI.escape(TVillion::TorrentSearch::THORRENTS_URL % "Futurama 720p S07E04"))).once
-      @resp.stub(:body).and_return(File.open("spec/data/futurama_torrent_search.json", "r").read)
+      @resp.stub(:body).and_return(File.open("spec/data/futurama_torrent_search_thorrents.json", "r").read)
       @resp.should_receive(:body).once
 
-      url = @torrent_search.get_search_results("Futurama 720p S07E04")
+      url = @torrent_search.search_thorrents("Futurama 720p S07E04")
       url.should eq("magnet:?xt=urn:btih:f647750b368f950f86ded488ac3160e3f5e30101&dn=Futurama+S07E26+720p+HDTV+x264-EVOLVE+%5Beztv%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337")
+    end
+
+    it "should parse fenopy search json and get a proper torrent url as a result" do
+      Net::HTTP.should_receive(:get_response).with(URI.parse(URI.escape(TVillion::TorrentSearch::FENOPY_URL % "NCIS 720p S11E11"))).once
+      @resp.stub(:body).and_return(File.open("spec/data/ncis_torrent_search_fenopy.json", "r").read)
+      @resp.should_receive(:body).once
+
+      url = @torrent_search.search_fenopy("NCIS 720p S11E11")
+      url.should eq("magnet:?xt=urn:btih:1446cee021f624870d61117dc00269fffe8a81ce&dn=NCIS+S11E11+720p+HDTV+X264+DIMENSION+PublicHD")
     end
   end
 end
