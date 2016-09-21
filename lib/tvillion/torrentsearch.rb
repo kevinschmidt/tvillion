2647c7c96d38d97ddfe4aff1e968c2298ba32b2a
Kevin Schmidt <kevin@kevinschmidt.biz>
2016-09-21 21:07:34 +0100

    code cleanup, switch search

--- lib/tvillion/torrentsearch.rb
+++ lib/tvillion/torrentsearch.rb
@@ -6,7 +6,7 @@ require "rexml/document"
 module TVillion
   module TorrentSearch
     def get_search_results(search_string)
-      search_kickass(search_string)
+      search_zooqle(search_string)
     end
 
     THORRENTS_URL = "http://thorrents.com/search/%s.json"
@@ -22,29 +22,17 @@ module TVillion
       return nil
     end
 
-    FENOPY_URL = "http://fenopy.se/module/search/api.php?keyword=%s&sort=relevancy&format=json&limit=10&category=78"
+    ZOOQLE_URL = "https://zooqle.com/search?q=%s&fmt=rss"
 
-    def search_fenopy(search_string)
-      resp = Net::HTTP.get_response(URI.parse(URI.escape(FENOPY_URL % search_string)))
-      data = resp.body
-      result = JSON.parse(data)
-      if not result.empty?
-        return CGI.unescapeHTML(result[0]['magnet'])
-      end
-      return nil
-    end
-
-    KICKASS_URL = "https://kat.cr/usearch/%s/?rss=1&field=seeders&sorder=desc"
-
-    def search_kickass(search_string)
-      uri = URI.parse(URI.escape(KICKASS_URL % search_string))
+    def search_zooqle(search_string)
+      uri = URI.parse(URI.escape(ZOOQLE_URL % search_string))
       net_http = Net::HTTP.new(uri.host, uri.port)
       net_http.use_ssl = true
       resp = net_http.get(uri.request_uri)
       data = resp.body
 
       begin
-        raise "got a bad response from kickass: " + resp.message if resp.code != "200"
+        raise "got a bad response from zooqle: " + resp.message if resp.code != "200"
         result = REXML::Document.new(data)
         return REXML::XPath.first(result, "//channel/item[1]/torrent:magnetURI").text
       rescue => ex
